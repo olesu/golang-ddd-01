f251dc529c23963be94dd63ae4a10f88f355c1da
Ole Kristian Sunde <ole.sunde@gmail.com>
2023-05-29 16:44:44 +0000

    add test case for failing card

--- cmd/main_test.go
+++ cmd/main_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"errors"
 	"testing"
 
 	"github.com/Rhymond/go-money"
@@ -10,15 +11,33 @@ import (
 )
 
 func TestRun(t *testing.T) {
-	app := appContext{
-		stripeService: stripeHandlerFunc(fakeStripeService),
-		purchaseRepo:  fakePurchaseRepo{},
-		storeRepo:     fakeStoreRepo{},
+	testCases := []struct {
+		name    string
+		cardErr error
+		want    int
+	}{
+		{
+			name: "all good",
+		},
+		{
+			name:    "failing card payment",
+			cardErr: errors.New("payment failed"),
+			want:    1,
+		},
 	}
-	got := run(context.Background(), app)
-	want := 0
-	if got != want {
-		t.Errorf("unexpected failure, got %d, want %d", got, want)
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			app := appContext{
+				stripeService: stripeHandlerFunc(fakeStripeService(tc.cardErr)),
+				purchaseRepo:  fakePurchaseRepo{},
+				storeRepo:     fakeStoreRepo{},
+			}
+			got := run(context.Background(), app)
+			if got != tc.want {
+				t.Errorf("unexpected failure, got %d, want %d", got, tc.want)
+			}
+		})
 	}
 }
 
@@ -28,20 +47,22 @@ func (f stripeHandlerFunc) ChargeCard(ctx context.Context, amount money.Money, c
 	return f(ctx, amount, cardToken)
 }
 
-func fakeStripeService(_ context.Context, _ money.Money, _ string) error {
-	return nil
+func fakeStripeService(wantErr error) stripeHandlerFunc {
+	return func(_ context.Context, _ money.Money, _ string) error {
+		return wantErr
+	}
 }
 
 type fakePurchaseRepo struct {
 }
 
-func (r fakePurchaseRepo) Store(ctx context.Context, purchase purchase.Purchase) error { return nil }
-func (r fakePurchaseRepo) Ping(ctx context.Context) error                              { return nil }
+func (r fakePurchaseRepo) Store(_ context.Context, _ purchase.Purchase) error { return nil }
+func (r fakePurchaseRepo) Ping(_ context.Context) error                       { return nil }
 
 type fakeStoreRepo struct {
 }
 
-func (r fakeStoreRepo) GetStoreDiscount(ctx context.Context, storeID uuid.UUID) (float32, error) {
+func (r fakeStoreRepo) GetStoreDiscount(_ context.Context, _ uuid.UUID) (float32, error) {
 	return 0, nil
 }
-func (r fakeStoreRepo) Ping(ctx context.Context) error { return nil }
+func (r fakeStoreRepo) Ping(_ context.Context) error { return nil }
